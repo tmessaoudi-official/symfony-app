c2db815c0a737658f8d79fbc1f4feaa917bfd99b
Takieddine Messaoudi <takieddine.messaoudi.official@gmail.com>
2020-12-06 17:35:22 +0100

    cs fix

--- src/Doctrine/Migrations/Version20201206161441.php
+++ src/Doctrine/Migrations/Version20201206161441.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * Personal project using Php 8/Symfony 5.2.x@dev.
+ *
+ * @author       : Takieddine Messaoudi <takieddine.messaoudi.official@gmail.com>
+ * @organization : Smart Companion
+ * @contact      : takieddine.messaoudi.official@gmail.com
+ *
+ */
+
 declare(strict_types=1);
 
 namespace App\Doctrine\Migrations;
@@ -12,12 +21,12 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20201206161441 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE refresh_token RENAME INDEX uniq_c74f2195c74f2195 TO UNIQ_C74F21955B4499FC');
@@ -25,7 +34,7 @@ final class Version20201206161441 extends AbstractMigration
         $this->addSql('ALTER TABLE user RENAME INDEX uniq_8d93d649e7927c74 TO UNIQ_8D93D6496F279BB4');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE `refresh_token` RENAME INDEX uniq_c74f21955b4499fc TO UNIQ_C74F2195C74F2195');
--- src/Doctrine/ORM/Mapping/QuoteStrategy.php
+++ src/Doctrine/ORM/Mapping/QuoteStrategy.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * Personal project using Php 8/Symfony 5.2.x@dev.
+ *
+ * @author       : Takieddine Messaoudi <takieddine.messaoudi.official@gmail.com>
+ * @organization : Smart Companion
+ * @contact      : takieddine.messaoudi.official@gmail.com
+ *
+ */
+
+declare(strict_types=1);
+
 namespace App\Doctrine\ORM\Mapping;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
@@ -10,12 +21,12 @@ class QuoteStrategy implements IQuoteStrategy
 {
     public function getColumnName($fieldName, ClassMetadata $class, AbstractPlatform $platform)
     {
-        return '`' . $class->fieldMappings[$fieldName]['columnName'] . '`';
+        return '`'.$class->fieldMappings[$fieldName]['columnName'].'`';
     }
 
     public function getTableName(ClassMetadata $class, AbstractPlatform $platform)
     {
-        return '`' . $class->table['name'] . '`';
+        return '`'.$class->table['name'].'`';
     }
 
     public function getSequenceName(array $definition, ClassMetadata $class, AbstractPlatform $platform)
@@ -35,7 +46,7 @@ class QuoteStrategy implements IQuoteStrategy
 
     public function getJoinTableName(array $association, ClassMetadata $class, AbstractPlatform $platform)
     {
-        return '`' . $association['joinTable']['name'] . '`';
+        return '`'.$association['joinTable']['name'].'`';
     }
 
     public function getIdentifierColumnNames(ClassMetadata $class, AbstractPlatform $platform)
@@ -45,6 +56,6 @@ class QuoteStrategy implements IQuoteStrategy
 
     public function getColumnAlias($columnName, $counter, AbstractPlatform $platform, ClassMetadata $class = null)
     {
-        return $platform->getSQLResultCasing($columnName . '_' . $counter);
+        return $platform->getSQLResultCasing($columnName.'_'.$counter);
     }
 }
